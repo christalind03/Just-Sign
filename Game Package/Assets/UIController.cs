180e81bb801e569c9421f0f532c1abcd9f8225a3
Jessica Nam <jessicanam@nevada.unr.edu>
2023-07-31 19:07:49 -0500

    Added Song1 button functionality

---
+++ Game Package/Assets/UIController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public Button Song1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        Song1 = root.Q<Button>("Song1");
+        Song1.clicked += Song1ButtonPressed;
+    }
+
+    void Song1ButtonPressed(){
+        SceneManager.LoadScene("Playground");
+    }
+}
