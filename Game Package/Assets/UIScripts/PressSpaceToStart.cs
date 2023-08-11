4dfeaea8c8e0ace64f6d0acf9ad0ac9e35fe6c8a
Jessica Nam <jessicanam@nevada.unr.edu>
2023-08-11 01:13:05 -0500

    game over scene done

--- Game Package/Assets/UIScripts/PressSpaceToStart.cs
+++ Game Package/Assets/UIScripts/PressSpaceToStart.cs
@@ -3,12 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
+    private string currentSceneName;
+
+    private void Start()
+    {
+        currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("SongSelectionMenu"); 
+        }
+
+        if (Input.GetMouseButtonDown(0) && currentSceneName == "GameOver")
+        {
+            // Load the SongSelectionMenu scene
+            SceneManager.LoadScene("SongSelectionMenu");
         }
     }
 }
