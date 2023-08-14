06463bcf5c65d3bee3064914446609dda8d9f2d3
Jessica Nam <jessicanam@nevada.unr.edu>
2023-08-14 12:36:22 -0700

    search functionality implemented

--- Game Package/Assets/SongSearch.cs
+++ Game Package/Assets/SongSearch.cs
@@ -1,25 +1,35 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using TMPro;
 
 public class SongSearch : MonoBehaviour
 {
-    public InputField searchInputField;
+    public TMP_InputField searchInputField; 
     public List<Button> songButtons = new List<Button>();
+    public GameObject searchPanel; // Add this for the search panel reference
 
     private void Start()
     {
+        // Initially hide the search panel
+        searchPanel.SetActive(false);
+        
         // Attach input field event
         searchInputField.onValueChanged.AddListener(FilterSongList);
+    }
+
+    // Add this method to toggle the search panel
+    public void ToggleSearchPanel()
+    {
+        searchPanel.SetActive(!searchPanel.activeSelf);
     }
 
     void FilterSongList(string query)
     {
         foreach (Button button in songButtons)
         {
-            // Check if the button's label contains the search query
-            Text buttonText = button.GetComponentInChildren<Text>();
-            if (buttonText.text.ToLower().Contains(query.ToLower()))
+            // Using the button's name for filtering
+            if (button.gameObject.name.ToLower().Contains(query.ToLower()))
             {
                 button.gameObject.SetActive(true);
             }
