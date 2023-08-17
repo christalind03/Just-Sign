3d943dbfbd0c252311f8ce200c12abeabe3ba812
Jessica Nam <jessicanam@nevada.unr.edu>
2023-08-17 00:58:11 -0700

    High score calculation using PlayerPref

--- Game Package/Assets/Scripts/GameOverController.cs
+++ Game Package/Assets/Scripts/GameOverController.cs
@@ -8,6 +8,7 @@ public class GameOverController : MonoBehaviour
 {
     public Image rankImage; // Drag and drop the Image component where you want to display rank
     public TextMeshProUGUI scoreText; // Drag and drop the Text component where you want to display the score
+    public TextMeshProUGUI highScoreText; // Drag and drop the Text component where you want to display the high score
 
     public Sprite rankS; // Assign the sprite for rank S in the inspector
     public Sprite rankA; // Assign the sprite for rank A in the inspector
@@ -46,6 +47,18 @@ public class GameOverController : MonoBehaviour
         }
 
         scoreText.text = "Score: " + totalScore.ToString();
+
+        // Check and update high score
+        int currentHighScore = PlayerPrefs.GetInt("HighScore", 0); // If "HighScore" doesn't exist, it defaults to 0
+        if (totalScore > currentHighScore)
+        {
+            PlayerPrefs.SetInt("HighScore", totalScore); // Save the new high score
+            PlayerPrefs.Save(); // Important to ensure that the data is saved to disk
+        }
+
+        // Display the high score
+        int savedHighScore = PlayerPrefs.GetInt("HighScore", 0); // If "HighScore" doesn't exist, it defaults to 0
+        highScoreText.text = "High Score: " + savedHighScore.ToString();
 
     }
 }
